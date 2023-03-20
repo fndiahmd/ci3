b01a43611b86e9f330638139b48e637acb4e81a7
fndiahmd <fandiaja101@gmail.com>
2023-03-20 07:56:51 +0700

    first commit

---
+++ application/controllers/Auth/BlockedController.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class BlockedController extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+		//Load Dependencies
+
+	}
+
+	// List all your items
+	public function index()
+	{
+		$data['title'] = 'Blocked';
+		$this->load->view('_part/backend_head', $data);
+		$this->load->view('blocked');
+		$this->load->view('_part/backend_footer_v');
+		$this->load->view('_part/backend_foot');
+	}
+}
+
+/* End of file BlockedController.php */
+/* Location: ./application/controllers/Auth/BlockedController.php */
